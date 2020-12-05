06de28ac90ff9483fffc49ee756200ad777e9570
meriambs <bensalah.meriem0@gmail.com>
2020-12-05 21:13:39 +0100

    last part

--- src/Component/Profile/Address.js
+++ src/Component/Profile/Address.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BsFillHouseFill } from "react-icons/bs";
 import { BsBriefcase } from "react-icons/bs";
 
-import { DiGithubBadge } from "react-icons/di";
+import { SiGithub } from "react-icons/si";
 import { DiJsBadge }from "react-icons/di";
 function Address() {
   return (
@@ -13,27 +13,32 @@ function Address() {
      <div style={{marginRight: '23px'}} > 
        <  BsFillHouseFill/>
        </div>
-       <p>adress place</p>
+       <p>Cite Olympique , Cite khadra </p>
+       <p>BP 1003 rue ......</p>
    </div>
 
    <div className="icon-part" style={{display:'flex',justifyContent:'space-between',backgroundColor: '#292c2f',boxShadow: '0 1px 1px 0 rgba(0, 0, 0, 0.12)',boxsizing: 'border-box',width: '100%',textAlign: 'left',font: 'bold 16px sans-serif',padding: '55px 50px',marginTop: '80px'}}>
      <div style={{display:'flex',flexWrap:'nowrap',color:"white"}}>
-        <div style={{height: '30px',width:' 61px'}}  >
+        <div    >
        <BsBriefcase ></BsBriefcase>
        </div>
-      <h3 style={{marginLeft: '20px'}} >mailpart</h3>
+      <h5 style={{marginLeft: '20px'}} >bensalah.meriem0@gmail.com</h5>
       </div>
       <div style={{display:'flex',flexWrap:'nowrap',color:"white"}} >
-        <div style={{height: '40px',width:' 61px'}}  >
-         <DiGithubBadge/>
+        <div    >
+          <a  href="https://github.com/meriambs" >
+         <SiGithub/>
+         </a>
          </div>
-      <h3 style={{marginLeft: '20px'}} >gitpart</h3>
+      <h5 style={{marginLeft: '20px'}} >gitpart</h5>
       </div>
       <div style={{display:'flex',flexWrap:'nowrap',color:"white"}} >
-        <div style={{height: '30px',width:' 71px'}}  >
+        <div   >
+          <a  href="https://www.codewars.com/users/MeriambenS">
         <DiJsBadge/>
+        </a>
         </div>
-      <h3 style={{    marginLeft: '20px'}} >codewars</h3>
+      <h5 style={{    marginLeft: '20px'}} >codewars</h5>
       </div>
      
   
--- src/Component/Profile/ProfilPhoto.js
+++ src/Component/Profile/ProfilPhoto.js
@@ -3,10 +3,10 @@ import React from 'react';
 function ProfilPhoto() {
   return (
     <div className="ProfilPhoto">
-    <header>
+    <header style={{display: 'flex',justifyContent: 'center'}} >
      
       
-     <img  src="./img/photo-de-profil.jpg" style={{height: '225px',boxShadow: '0 4px 8px 0 rgba (0, 0, 0, 0.2) 0 6px 20px 0 rgba (0, 0, 0, 0.19)',borderRadius: '109px',marginLeft: '594px',marginTop: '42px'}} />
+     <img  src="./img/photo-de-profil.jpg" style={{height: '225px',boxShadow: '0 4px 8px 0 rgba (0, 0, 0, 0.2) 0 6px 20px 0 rgba (0, 0, 0, 0.19)',borderRadius: '109px'}} />
    
 
     </header>
